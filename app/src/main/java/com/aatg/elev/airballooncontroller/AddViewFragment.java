c58928c6461f81d2fb1f4a1f4c8f640dea82d1b4
c3lphie <simon_sc@live.dk>
2019-01-05 20:59:06 +0100

    spinners now working

--- app/src/main/java/com/aatg/elev/airballooncontroller/AddViewFragment.java
+++ app/src/main/java/com/aatg/elev/airballooncontroller/AddViewFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 import com.aatg.elev.airballooncontroller.R;
 
 public class AddViewFragment extends Fragment {
@@ -16,21 +18,28 @@ public class AddViewFragment extends Fragment {
         // Required empty public constructor
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState){
-        Spinner module_type = (Spinner) findViewById(R.id.module_type);
-        ArrayAdapter<CharSequence> adapter_type = ArrayAdapter.createFromResource(this,R.array.module_types,android.R.layout.simple_spinner_item);
-        adapter_type.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        module_type.setAdapter(adapter_type);
-    }
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_view, container, false);
+
+        //Module type
+        Spinner module_type = view.findViewById(R.id.module_type);
+        ArrayAdapter<CharSequence> adapter_type = ArrayAdapter.createFromResource(getContext(), R.array.module_types, android.R.layout.simple_spinner_item);
+        adapter_type.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        module_type.setAdapter(adapter_type);
+
+        //Module port
+        Spinner module_port = view.findViewById(R.id.module_port);
+        ArrayAdapter<CharSequence> adapter_port = ArrayAdapter.createFromResource(getContext(), R.array.port_number, android.R.layout.simple_spinner_item);
+        adapter_port.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        module_port.setAdapter(adapter_port);
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_view, container, false);
+        return view;
     }
 
 }
